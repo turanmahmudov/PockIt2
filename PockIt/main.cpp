36f1a56bdc64023e650dadc5616a4890a8ce98e8
Turan Mahmudov <turan.mahmudov@gmail.com>
2016-11-04 15:32:06 +0300

    Initial qmake project

---
+++ PockIt/main.cpp
@@ -0,0 +1,14 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQuickView>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    QQuickView view;
+    view.setSource(QUrl(QStringLiteral("qrc:///Main.qml")));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    view.show();
+    return app.exec();
+}
